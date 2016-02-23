91921fdd80c34b184c317d8c8ec6be6f413e2912
Keith To <keithtkto@gmail.com>
2016-02-22 21:11:06 -0800

    create new company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,36 @@
 class CompaniesController < ApplicationController
+  def index
+    @companies = Company.all
+  end
+
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+
+    if @company.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+private
+  def company_params
+    params.require(:company).permit(
+      :name,
+      :field,
+      :country,
+      :timezone,
+      :week_start_day,
+      :owner_id
+      )
+  end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,13 @@
 class Company < ActiveRecord::Base
   has_many :users
-  # validates :name, presence: true
+  validates :name, presence: true
   # validates :owner_id, presence: true
   # validates :timezone, presence: true
+
+
+  # def country_name
+  #   country = ISO3166::Country[country_code]
+  #   country.translations[I18n.locale.to_s] || country.name
+  # end
 end
 
