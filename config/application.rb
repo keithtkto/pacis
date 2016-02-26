353452a4e639c7f00e57411fa5a38059469bea76
Keith To <keithtkto@gmail.com>
2016-02-25 23:55:17 -0800

    attempt to deploy to heroku

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,4 @@ module Pacis
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
-    config.assets.initialize_on_precompile = false
 end
