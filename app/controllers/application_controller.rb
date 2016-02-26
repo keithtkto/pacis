f1076d7a163bc77516b2cb5cc9046e53b98b3dcc
Keith To <keithtkto@gmail.com>
2016-02-25 19:18:11 -0800

    add side bar and correct css on log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,11 @@ private
   helper_method :current_company
 
   def current_user
-    current_user ||= User.find(session[:user_id]) if session[:user_id]
+    current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
   end
 
   def current_company
-    current_company ||= Company.find(current_user.company_id)
+    current_company ||= Company.find_by(id: current_user.company_id)
   end
 
 
